4afbd0ca93cb1a86252f9de689e121a35d28ca17
Ruri Meiko <chandoralong@.com>
2024-01-03 16:46:02 +0700

    okx

--- src/bing/bing@imgcreater.ts
+++ src/bing/bing@imgcreater.ts
@@ -15,21 +15,14 @@ export default class BingImageCreater {
 		method: string = "GET",
 		body: string | null = null
 	) {
-		const randomIP: string = `13.${Math.floor(Math.random() * 4) + 104}.${Math.floor(
-			Math.random() * 256
-		)}.${Math.floor(Math.random() * 256)}`;
-
 		const defaultOptions: any = {
 			headers: {
 				accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 				"accept-language": "en-US,en;q=0.9",
 				"cache-control": "max-age=0",
 				"content-type": "application/x-www-form-urlencoded",
-				referrer: "https://www.bing.com/images/create/",
-				origin: "https://www.bing.com",
-				"user-agent":
-					"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.63",
-				"x-forwarded-for": randomIP,
+				"sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+				"Referrer-Policy": "origin-when-cross-origin",
 			},
 			body: body,
 			method: method,
